9b95638fae1a35acdc89dda8df698b22ef0365f8
NikaNatt <fillvero@yandex.ru>
2024-09-21 15:17:59 +0300

    Create base widgets classes: HUD, WidgetContoller, UserWidget

---
+++ Source/AgroTechAvia/Public/UI/WidgetController/ATA_WidgetController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ATA_WidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGROTECHAVIA_API UATA_WidgetController : public UObject
+{
+	GENERATED_BODY()
+	
+};
